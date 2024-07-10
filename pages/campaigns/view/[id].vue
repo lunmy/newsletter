21a193af4232c8d022da43d4a2edcd8ce5f1c513
NaloK7 <129076162+NaloK7@users.noreply.github.com>
2024-07-10 14:33:32 +0200

    add search field for campaign and newsletter

--- pages/campaigns/view/[id].vue
+++ pages/campaigns/view/[id].vue
@@ -1,62 +1,127 @@
 <template>
   <h1 class="p-4 text-2xl font-bold text-center">Campaigns</h1>
+
   <v-data-table
     v-model:expanded="expanded"
     :headers="headers"
     :items="campaignsList"
+    :search="searchCampText"
     item-value="name"
     expand-on-click
     show-expand
     :items-per-page-options="items_per_page"
     :loading="loading">
-    <!-- loader template -->
+    <!-- loader  -->
     <template v-slot:loading>
       <span v-for="i in [...Array(5).keys()]">
         <v-skeleton-loader type="text" width="400"></v-skeleton-loader>
         <v-skeleton-loader type="divider"></v-skeleton-loader>
       </span>
     </template>
-    <!-- slot to access principal row-->
+
+    <!-- header  -->
+    <template v-slot:headers="{ columns, isSorted, getSortIcon, toggleSort }">
+      <tr>
+        <template v-for="column in columns" :key="column.key">
+          <td>
+            <span class="mr-2 flex">
+              <template v-if="isSorted(column)">
+                <v-icon :icon="getSortIcon(column)"></v-icon>
+              </template>
+              <p
+                class="my-auto mr-2 cursor-pointer"
+                @click="() => toggleSort(column)">
+                {{ column.title }}
+              </p>
+              <!-- name column -->
+              <span v-if="column.key == 'name'" class="flex">
+                <v-icon
+                  icon="mdi-magnify"
+                  @click="toggleSearchCamp()"
+                  start
+                  size="large"
+                  class="my-auto cursor-pointer"></v-icon>
+                <!-- search campaign field -->
+                <v-text-field
+                  v-if="searchCamp"
+                  v-model="searchCampText"
+                  @blur="toggleSearchCamp()"
+                  update:focused="true"
+                  label="Search"
+                  variant="outlined"
+                  hide-details
+                  single-line
+                  density="compact"
+                  class="max-w-96 w-96"></v-text-field>
+              </span>
+            </span>
+          </td>
+        </template>
+      </tr>
+    </template>
+    <!-- items: campaings  -->
     <template v-slot:item.name="{ item }">
       <td class="font-semibold">
         {{ item.name }} ({{ item["newsletters"].length }})
       </td>
     </template>
-    <!-- slot to access extended row -->
+    <!-- EXPANDED ROW -->
     <template v-slot:expanded-row="{ item }">
       <!-- sub table -->
       <v-data-table
         :items="item['newsletters']"
         :headers="subHeaders"
+        :search="searchNewsText"
         class="pl-10"
         hide-default-footer
         items-per-page="-1">
+        <!-- header  -->
         <template
           v-slot:headers="{ columns, isSorted, getSortIcon, toggleSort }">
           <tr>
             <template v-for="column in columns" :key="column.key">
               <td>
-                <span
-                  class="mr-2 cursor-pointer font-semibold"
-                  @click="() => toggleSort(column)"
-                  >{{ column.title }}</span
-                >
-                <template v-if="isSorted(column)">
-                  <v-icon :icon="getSortIcon(column)"></v-icon>
-                </template>
-                <v-icon
-                  v-if="column.removable"
-                  icon="$close"
-                  @click="() => remove(column.key)"></v-icon>
+                <span class="mr-2 flex">
+                  <template v-if="isSorted(column)">
+                    <v-icon :icon="getSortIcon(column)"></v-icon>
+                  </template>
+                  <p
+                    class="my-auto mr-2 cursor-pointer"
+                    @click="() => toggleSort(column)">
+                    {{ column.title }}
+                  </p>
+                  <!-- name column -->
+                  <span v-if="column.key == 'name'" class="flex">
+                    <v-icon
+                      icon="mdi-magnify"
+                      @click="toggleSearchNews()"
+                      start
+                      size="large"
+                      class="my-auto cursor-pointer"></v-icon>
+                    <!-- search campaign field -->
+                    <v-text-field
+                      v-if="searchNews"
+                      v-model="searchNewsText"
+                      @blur="toggleSearchNews()"
+                      label="Search"
+                      variant="outlined"
+                      hide-details
+                      single-line
+                      density="compact"
+                      class="max-w-96 w-96"></v-text-field>
+                  </span>
+                </span>
               </td>
             </template>
           </tr>
         </template>
+        <!-- items: newsletters name -->
         <template v-slot:item.name="{ item }">
           <NuxtLink :to="'/newsletters/view/' + getIdFromIri(item['@id'])">
             {{ item.name.toUpperCase() }}
           </NuxtLink>
         </template>
+        <!-- items: newsletters createdAt -->
         <template v-slot:item.createdAt="{ item }">
           {{ $moment(item.createdAt).format("DD/MM/YYYY") }}
         </template>
@@ -72,6 +137,10 @@ const route = useRoute();
 const { $moment } = useNuxtApp();
 
 const loading = ref(true);
+const searchCampText = ref("");
+const searchNewsText = ref("");
+const searchCamp = ref(false);
+const searchNews = ref(false);
 const updatePath = ref("");
 const campaignsList = ref([]);
 const expanded = ref([]);
@@ -99,6 +168,12 @@ const subHeaders = ref([
   },
 ]);
 
+function toggleSearchCamp() {
+  searchCamp.value = !searchCamp.value;
+}
+function toggleSearchNews() {
+  searchNews.value = !searchNews.value;
+}
 async function getCampaignsList() {
   try {
     const criteria = {
